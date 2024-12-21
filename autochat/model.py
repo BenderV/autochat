c80118f84d44f4af8c1f26a68d1872a3225a347b
Benjamin DERVILLE <benderville@gmail.com>
2024-12-21 17:15:28 +0100

    feat(message): add support for parts

--- autochat/model.py
+++ autochat/model.py
@@ -8,8 +8,9 @@ from PIL import Image as PILImage
 
 
 class FunctionCallParsingError(Exception):
-    def __init__(self, obj):
-        self.obj = obj
+    def __init__(self, id, function_call):
+        self.id = id
+        self.function_call = function_call
 
     def __str__(self):
         return f"Invalid function_call: {self.obj.function_call}"
@@ -53,7 +54,86 @@ class Image:
         return "image/" + self.image.format.lower()
 
 
+class MessagePart:
+    def __init__(
+        self,
+        type: Literal["text", "image", "function_call", "function_result"],
+        content: typing.Optional[str] = None,  # Should be named "text" ?
+        image: typing.Optional[PILImage.Image] = None,
+        function_call: typing.Optional[dict] = None,
+        function_call_id: typing.Optional[str] = None,
+        data: typing.Optional[dict] = None,
+    ) -> None:
+        self.type = type
+        self.content = content
+        self.image = Image(image) if image else None
+        self.function_call = function_call
+        self.function_call_id = function_call_id
+        self.data = data
+
+    def to_openai_dict(self) -> dict:
+        if self.type == "text":
+            return {
+                "type": "text",
+                "content": self.content,
+            }
+        elif self.type == "image":
+            return {
+                "type": "image_url",
+                "image_url": {
+                    "url": f"data:{self.image.format};base64,{self.image.to_base64()}"
+                },
+            }
+        elif self.type == "function_call":
+            return {
+                "name": self.function_call["name"],
+                "arguments": json.dumps(self.function_call["arguments"]),
+            }
+        elif self.type == "function_result":
+            return {
+                "type": "text",
+                "content": self.content,
+            }
+
+        return {}
+
+    def to_anthropic_dict(self) -> dict:
+        if self.type == "text":
+            return {
+                "type": "text",
+                "text": self.content,
+            }
+        elif self.type == "image":
+            return {
+                "type": "image",
+                "source": {
+                    "type": "base64",
+                    "media_type": self.image.format,
+                    "data": self.image.to_base64(),
+                },
+            }
+        elif self.type == "function_call":
+            return {
+                "type": "tool_use",
+                "id": self.function_call_id,
+                "name": self.function_call["name"],
+                "input": self.function_call["arguments"],
+            }
+        elif self.type == "function_result":
+            return {
+                "type": "tool_result",
+                "tool_use_id": self.function_call_id,
+                "content": self.content,
+            }
+        return {}
+
+
 class Message:
+    role: Literal["user", "assistant", "function"]
+    parts: list[MessagePart]
+    name: typing.Optional[str] = None
+    id: typing.Optional[int] = None
+
     def __init__(
         self,
         role: Literal["user", "assistant", "function"],
@@ -63,54 +143,99 @@ class Message:
         id: typing.Optional[int] = None,
         function_call_id: typing.Optional[str] = None,
         image: typing.Optional[PILImage.Image] = None,
-        data: typing.Optional[dict] = None,  # Used for function call output
+        parts: typing.Optional[list[MessagePart]] = [],
     ) -> None:
         self.role = role
-        self.content = content
         self.name = name
-        self.function_call = function_call
         self.id = id
-        self.function_call_id = function_call_id
-        self.image = Image(image) if image else None
-        self.data = data
+
+        # Can't have both parts and content/image/function_call
+        if parts and (content or image or function_call):
+            raise ValueError("Can't have both parts and content/image/function_call")
+
+        if parts:
+            self.parts = parts
+        else:
+            self.parts = []
+            if role != "function":
+                if content:
+                    self.parts.append(MessagePart(type="text", content=content))
+                if image:
+                    self.parts.append(MessagePart(type="image", image=image))
+                if function_call:
+                    self.parts.append(
+                        MessagePart(
+                            type="function_call",
+                            function_call=function_call,
+                            function_call_id=function_call_id,
+                        )
+                    )
+            else:
+                self.parts.append(
+                    MessagePart(
+                        type="function_result",
+                        content=content,
+                        function_call_id=function_call_id,
+                    )
+                )
+
+    @property
+    def content(self) -> str:
+        # If one part, then provide the sugar syntax
+        if len(self.parts) == 1 and self.parts[0].type == "text":
+            return self.parts[0].content
+        return None
+
+    @property
+    def function_call(self) -> typing.Optional[dict]:
+        # If one part, then provide the sugar syntax
+        if len(self.parts) == 1 and self.parts[0].type == "function_call":
+            return self.parts[0].function_call
+        return None
+
+    @property
+    def function_call_id(self) -> typing.Optional[str]:
+        # If one part, then provide the sugar syntax
+        if len(self.parts) == 1 and self.parts[0].type == "function_call":
+            return self.parts[0].function_call_id
+        return None
+
+    @property
+    def function_result(self) -> typing.Optional[str]:
+        # If one part, then provide the sugar syntax
+        if len(self.parts) == 1 and self.parts[0].type == "function_result":
+            return self.parts[0].content
+        return None
+
+    @property
+    def image(self) -> typing.Optional[PILImage.Image]:
+        # If one part, then provide the sugar syntax
+        if len(self.parts) == 1 and self.parts[0].type == "image":
+            return self.parts[0].image
+        return None
 
     def to_openai_dict(self) -> dict:
         res = {
             "role": self.role,
             "content": [],
         }
-        if self.content:
-            res["content"].append(
-                {
-                    "type": "text",
-                    "text": self.content,
-                }
-            )
-        if self.image:
-            res["content"].append(
-                {
-                    "type": "image_url",
-                    "image_url": {
-                        "url": f"data:{self.image.format};base64,{self.image.to_base64()}"
-                    },
-                }
-            )
+
+        for part in self.parts:
+            if part.type == "function_call" and self.role == "assistant":
+                res["function_call"] = part.to_openai_dict()
+            elif part.type == "function_call" and self.role == "user":
+                # Workaround: If the user is triggering a function, we add it's name and arguments to the content
+                res["content"] = (
+                    part.function_call["name"]
+                    + ":"
+                    + json.dumps(part.function_call["arguments"])
+                )
+            else:
+                res["content"].append(part.to_openai_dict())
+
         if self.name:
             res["name"] = self.name
-        if self.function_call:
-            if self.role == "assistant":
-                res["function_call"] = {
-                    "name": self.function_call["name"],
-                    "arguments": json.dumps(self.function_call["arguments"]),
-                }
-            else:
-                # If user is triggering a function, we add the function call to the content
-                # since openai doesn't support functions from user messages
-                res["content"] = (
-                    self.function_call["name"]
-                    + ":"
-                    + json.dumps(self.function_call["arguments"])
-                )
+
         return res
 
     def to_anthropic_dict(self) -> dict:
@@ -118,72 +243,32 @@ class Message:
             "role": self.role if self.role in ["user", "assistant"] else "user",
             "content": [],
         }
-        if self.role == "function":
-            if self.image:
-                content = [
-                    {
-                        "type": "image",
-                        "source": {
-                            "type": "base64",
-                            "media_type": self.image.format,
-                            "data": self.image.to_base64(),
-                        },
-                    }
-                ]
-            else:
-                content = self.content
 
-            res["content"].append(
-                {
-                    "type": "tool_result",
-                    "tool_use_id": self.function_call_id,
-                    "content": content,
-                }
-            )
-            return res
-
-        if self.content:
-            res["content"].append(
-                {
-                    "type": "text",
-                    "text": self.content,
-                }
-            )
-        if self.function_call:
-            res["content"].append(
-                {
-                    "type": "tool_use",
-                    "id": self.function_call_id,
-                    "name": self.function_call["name"],
-                    "input": self.function_call["arguments"],
-                }
-            )
-        if self.image:
-            res["content"].append(
-                {
-                    "type": "image",
-                    "source": {
-                        "type": "base64",
-                        "media_type": self.image.format,
-                        "data": self.image.to_base64(),
-                    },
-                }
-            )
+        for part in self.parts:
+            res["content"].append(part.to_anthropic_dict())
 
         return res
 
     @classmethod
-    def from_openai_dict(cls, **kwargs):
-        obj = cls(**kwargs)
-        if obj.function_call:
-            # Parse function_call with json.loads
+    def from_openai_dict(
+        cls,
+        role: str,
+        content: str,
+        function_call: typing.Optional[dict] = None,
+        id: typing.Optional[str] = None,
+    ):
+        # We need to unquote the arguments
+        function_call_dict = None
+        if function_call:
             try:
-                obj.function_call = {
-                    "name": obj.function_call.name,
-                    "arguments": json.loads(obj.function_call.arguments),
+                function_call_dict = {
+                    "name": function_call.name,
+                    "arguments": json.loads(function_call.arguments),
                 }
             except json.decoder.JSONDecodeError:
-                raise FunctionCallParsingError(obj)
+                raise FunctionCallParsingError(id, function_call)
+
+        obj = cls(role=role, content=content, function_call=function_call_dict, id=id)
         return obj
 
     @classmethod
@@ -230,22 +315,22 @@ class Message:
             raise ValueError("Unable to parse anthropic message")
 
     def __repr__(self) -> str:
-        text = f"Message(role={self.role}, "
-        if self.content:
-            text += f'content="{self.content}", '
-        if self.function_call:
-            text += f'function_call="{self.function_call}", '
-        return text[:-2] + ")"
+        text = f"Message(role={self.role}, parts=["
+        for part in self.parts:
+            text += f"{part.type}, "
+        return text[:-2] + "])"
 
     def to_markdown(self) -> str:
         text = f"## {self.role}\n"
-        if self.content is not None:
-            text += self.content + "\n"
-        if self.function_call is not None:
-            # Display function_call so it look like func(arg1="value1", arg2="value2")
-            text += f"> {self.function_call['name']}({', '.join([f'{k}={v}' for k, v in self.function_call['arguments'].items()])})\n"
-        if self.content is None and self.function_call is None:
-            raise ValueError("Message should have content or function_call")
+        for part in self.parts:
+            if part.type == "text":
+                text += part.content + "\n"
+            elif part.type == "function_call":
+                text += f"> {part.function_call['name']}({', '.join([f'{k}={v}' for k, v in part.function_call['arguments'].items()])})\n"
+            elif part.type == "function_result":
+                text += f"> Result: {part.content}\n"
+        if not self.parts:
+            raise ValueError("Message should have at least one part")
         return text
 
 
