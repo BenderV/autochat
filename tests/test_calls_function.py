efbd63d71d83b2770e56dc6041b0246cb386ba87
Benjamin <BenderV@users.noreply.github.com>
2025-04-14 22:47:04 +0200

    test: use pytest parametrize (#24)

--- tests/test_calls_function.py
+++ tests/test_calls_function.py
@@ -1,7 +1,7 @@
 import pytest
-from autochat import Autochat, APIProvider
 
-# Our "book" data
+from autochat import APIProvider, Autochat
+
 BOOKS = {
     "The Great Gatsby": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
     "To Kill a Mockingbird": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
@@ -17,29 +17,22 @@ def read_book(book_name: str):
     return BOOKS[book_name]
 
 
+TEST_CASES = [
+    (
+        APIProvider.ANTHROPIC,
+        "claude-3-7-sonnet-latest",
+    ),
+    (APIProvider.OPENAI, "gpt-4o"),
+]
+
+
+@pytest.mark.parametrize("provider, model", TEST_CASES)
 @pytest.mark.vcr
-def test_anthropic_read_book():
+def test_function_calling(provider: APIProvider, model: str):
     agent = Autochat(
         "You are a helpful assistant that can read books.",
-        #
-        provider=APIProvider.ANTHROPIC,
-    )
-    agent.add_function(list_books)
-    agent.add_function(read_book)
-
-    user_query = "Hello, read book 'the great gatsby'"
-    messages = []
-    for message in agent.run_conversation(user_query):
-        print(message.to_markdown())  # For debugging
-        messages.append(message)
-
-
-@pytest.mark.vcr
-def test_openai_read_book():
-    agent = Autochat(
-        "You are a helpful assistant that can read books.",
-        model="gpt-4o",
-        provider=APIProvider.OPENAI,
+        provider=provider,
+        model=model,
     )
     agent.add_function(list_books)
     agent.add_function(read_book)
